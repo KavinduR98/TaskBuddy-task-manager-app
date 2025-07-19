2465c1fec691143e75344054cbf614310a40410c
@kavinduushan07 <kavinduushan07@gmail.com>
2025-07-19 12:25:38 +0530

    create TopNavbar component

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import TopNavbar from './TopNavbar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <div className='flex h-screen bg-gray-100'>
         <Sidebar />
+        <div className='flex-1 flex flex-col overflow-hidden'>
+            <TopNavbar />
+            <main className='flex-1 overflow-x-hidden overflow-y-auto p-6'>
+              <Outlet />
+            </main>
+        </div>
     </div>
   )
 }
